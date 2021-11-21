98532c3fc12986ee8c04c0ad770e60a765bc08b0
TKunnapuu <tk.taavi@gmail.com>
2021-11-21 09:36:57 +0200

    testing

---
+++ src/components/footer.vue
@@ -0,0 +1,26 @@
+<template>    
+    <div id="foot" 
+    padless
+    color="primary lighten-1"
+>
+      <router-link to="/contact">Contact</router-link>
+      <p> 2021 </p>
+    </div>
+</template>
+
+<style lang="scss">
+    #foot {
+        a {
+            font-weight: bold;
+            color: #2c3e50;
+
+            &.router-link-exact-active {
+            color: #42b983;
+            }
+        }
+    }
+
+    p{
+        font-size: 10px;
+    }
+</style>
---
+++ src/components/post.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="post-list">
+    <div class="contentBox" v-for = "post in postList" :key ="post.id">
+            <div class="contentHeader">
+                <img src="@/assets/images/user.png">
+                <p class=date>
+                    {{post.date}}
+                </p>
+            </div>
+            <div class="contentContent">
+                <img class="imagePost" :src="require('@/assets/images/' + post.image + '.jpg')" alt=""/>
+
+                <p class = "caption">
+                    {{post.caption}} 
+                </p>
+            </div>
+            <div class="contentFooter">
+                <input class="like" type="image" :src="require('@/assets/images/like.jpg')" style="width:76;height:48px;"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "post",
+        data: function(){
+            return {}
+        },
+        computed: {
+            postList() {return this.$store.state.postList}
+        }
+    }
+</script>
