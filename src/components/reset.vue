898eab0c5d1141a39890372982c5e10414363602
ilmarvod <ilmar.vodi@ut.ee>
2021-11-22 00:00:10 +0200

    button

---
+++ src/components/reset.vue
@@ -0,0 +1,36 @@
+<template>    
+    <div id="rst" 
+    color="primary lighten-1"
+    >
+
+        <div class="contentFooter">
+                <input class="reset" type="image" :src="require('@/assets/images/reset.png')" style="width:76;height:48px;"/>
+            </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "post",
+        data: function(){
+            return {}
+        },
+        computed: {
+            postList() {return this.$store.state.postList}
+        }
+    }
+</script>
+
+<style>
+    input[type="image"] {
+        width: 50%;
+        height: 40px;
+        border-radius: 6px;
+        background: #0c5899;
+        color: white;
+        margin-bottom: 10px;
+    }
+    .reset:hover {
+    box-shadow: 2px 2px 2px 2px gray;}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
 <div class="home">
     <Head/>
+    <Reset/>
     <Post/>
     <Foot/>
   </div>
@@ -9,13 +10,14 @@
 <script>
 // @ is an alias to /src
 import Head from "@/components/header.vue";
+import Reset from "@/components/reset.vue";
 import Post from "@/components/post.vue";
 import Foot from "@/components/footer.vue";
 
 export default {
   name: "home",
   components: {
-    Post,Head,Foot
+    Post, Reset, Head,Foot
   },
 };
 </script>
