23198c6c500dcd76b94ce51dca522e170ecdef67
TKunnapuu <tk.taavi@gmail.com>
2021-11-21 10:07:47 +0200

    fix

--- src/components/post.vue
+++ src/components/post.vue
@@ -32,3 +32,38 @@
         }
     }
 </script>
+
+<style scoped>
+    .contentHeader{ /* Header of a post */
+        display: flex;
+        justify-content: space-between;
+        padding-bottom: 10px;
+    }
+    .contentHeader>p{
+    padding: 10px;
+    }
+    .contentHeader>img{
+        width: 50px;
+        height: 50px;
+    }
+    .contentBox{ /* The container for the contents of a post */
+        display: flex;
+        justify-content: center;
+        box-shadow: 5px 5px 5px gray;
+        margin: 20px;
+        flex-direction: column;
+        border: wheat;
+    }
+    input[type="submit"] {
+        width: 50%;
+        height: 40px;
+        border-radius: 6px;
+        background: #0c5899;
+        color: white;
+        margin-bottom: 10px;
+    }
+    .imagePost{ /* The post's image */
+    width: 100%;}
+    .like:hover {
+    box-shadow: 2px 2px 2px 2px gray;}
+</style>
